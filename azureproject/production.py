72e9c32f1a6a37cdc2b253f926652e0e10ea9b12
Chetna-DevOps <cp15091ece2015@gmail.com>
2026-06-20 21:39:04 +0530

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,4 +1,4 @@
-import os
+#import os
 
 # DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
 #     dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
@@ -6,3 +6,15 @@ import os
 #     dbhost=os.getenv('AZURE_POSTGRESQL_HOST'),
 #     dbname=os.getenv('AZURE_POSTGRESQL_NAME')
 # )
+
+import os
+
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'],
+    dbname=os.environ['DBNAME']
+)
+
+CACHE_TYPE = 'RedisCache'
+CACHE_REDIS_URL = os.environ.get('CACHELOCATION')
